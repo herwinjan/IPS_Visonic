e226f5aabcbae975de3603bd046e587ae6e4cda5
Herwin Jan Steehouwer <herwin@steehouwer.nu>
2017-05-20 08:47:45 +0200

    first test

--- Visonic/module.php
+++ Visonic/module.php
@@ -1,3 +1,47 @@
 <?
+    //
+    class IPS_Visonic extends IPSModule {
 
+        // The constructor of the module
+        // Overrides the default constructor of IPS
+        public  function __construct ( $ InstanceID )  {
+            // Do not delete this row
+            parent::__construct ( $ InstanceID ) ;
+
+            // Self-service code
+        }
+
+        // Overrides the internal IPS_Create ($ id) function
+        public  function Create ( )  {
+            // Do not delete this row.
+            parent::Create ( ) ;
+
+        }
+
+        // Overrides the intere IPS_ApplyChanges ($ id) function
+        public  function ApplyChanges ( )  {
+            // Do not delete this line
+            parent::ApplyChanges ( ) ;
+        }
+
+
+        public function ReceiveData($JSONString)
+        {
+             $data = json_decode($JSONString);
+             IPS_LogMessage("Visonic", utf8_decode($data->Buffer));
+             //Parse and write values to our variables
+
+        }
+
+        / **
+        * The following functions are automatically available when the module has been inserted via the "Module Control".
+        * The functions, with the self-defined prefix, are provided in PHP and JSON-RPC as follows:
+        *
+        * ABC_MyFirstElement ($ id);
+        *
+        * /
+        Public function setStatus($status)  {
+            // Self-definedCode
+        }
+    }
 ?>
