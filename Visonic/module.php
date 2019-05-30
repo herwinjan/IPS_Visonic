0da08701c0153a8dfea6dd2fe877d1a137c9b06b
Herwin Jan Steehouwer <herwin@steehouwer.nu>
2019-05-30 16:57:11 +0200

    debug test

--- Visonic/module.php
+++ Visonic/module.php
@@ -295,7 +295,7 @@ class VisonicAlarmDevice extends IPSModule
     public function ReceiveData($JSONString)
     {
         $data = json_decode($JSONString);
-        // IPS_LogMessage("Visonic RERV", utf8_decode($data->Buffer));
+        IPS_LogMessage("Visonic RERV", utf8_decode($data->Buffer));
 
         $dt = json_decode($data->Buffer, true);
 
